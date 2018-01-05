3f48b070d251e4ce861dbc0c2b04b33d7e71391d
DoubleMagicXu <2528167446@qq.com>
2018-01-05 15:24:48 +0000

    流水作业调度

--- 流水作业调度.cpp
+++ 流水作业调度.cpp
@@ -6,6 +6,33 @@ n1{i|ai<bi}   n2{i|ai>=bi};
 n1按ai非减序排列１，n2按bi非增排列
 n1中作业接n2中作业
 */
+template <class Type>
+int partition(Type *a,int p,int r)
+{
+  int i=p;
+  int j=r+1;
+  Type x=a[p];
+  while(true)
+  {
+    while(a[++i]<=x&&i<r);
+    while(x<=a[--j]);
+    if(i>=j)break;
+    swap(a[i],a[j]);
+  }
+  a[p]=a[j];
+  a[j]=x;
+  return j;
+}
+template <class Type>
+void quickSort(Type *a,int p,int r)
+{
+  if(p<r)
+  {
+    int k=partition(a,p,r);
+    quickSort(a,p,k-1);
+    quickSort(a,k+1,r);
+  }
+}
 int flowShop(int n,int *a,int *b,int *c )
 {
   class JOb
@@ -26,7 +53,8 @@ int flowShop(int n,int *a,int *b,int *c )
     d[i].job=a[i]<b[j];
     d[i].index=i;
   }
-  sort(d,n);
+  //sort(d,n);
+  quickSort(d,0,n-1);
   int p=0;
   int r=n-1;
   for(int i=0;i<n;i++)
